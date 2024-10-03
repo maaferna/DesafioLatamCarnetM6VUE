562a3cefea4b62ebc78cef54823177a9b6203cc0
mparraf <maaferna@gmail.com>
2024-10-02 23:04:03 -0300

    Se finaliza proyecto queda listo para subir a la platafoma

--- carnet_vue_vite/src/components/carnet.vue
+++ carnet_vue_vite/src/components/carnet.vue
@@ -1,89 +1,179 @@
 <template>
-    <div class="carnet">
-      <aside>
-        <div>
-          <span>CEDULA DE INDENTIDAD</span>
-          <img width="40" src="../assets/img/chile_bandera.png" alt="Chile Flag" />
+  <div class="container">
+    <div class="row carnet">
+      <div class="col-4 carnet-aside">
+        <div class="header d-flex justify-content-between">
+          <h5>CEDULA DE INDENTIDAD</h5>
+          <img id="chile-flag" src="../assets/img/chile_bandera.png" alt="Chile Flag" />
         </div>
-        <img src="../assets/img/foto_elon.jpg" alt="User Photo" />
-        <p>RUN {{ run }}</p>
-      </aside>
-  
-      <main>
+        <img class="user-photo" src="../assets/img/foto_elon.jpg" alt="User Photo" />
+        <p class="run">RUN {{ run }}</p>
+      </div>
+
+      <div class="col-8 carnet-main">
         <header>
-          <p>REPUBLICA DE CHILE</p>
-          <span>SERVICIO DE REGISTRO CIVIL E IDENTIFICACIÓN</span>
+          <h1 class="text-left">REPUBLICA DE CHILE</h1>
+          <h6 class="servicio text-left">SERVICIO DE REGISTRO CIVIL E IDENTIFICACIÓN</h6>
         </header>
+
         <section>
-          <article>
-            <header>APELLIDOS</header>
-            <span>{{ apellidos }}</span>
-          </article>
-          <article>
-            <header>NOMBRES</header>
-            <span>{{ nombres }}</span>
-          </article>
-  
-          <div class="grid-2-colums">
-            <article>
-              <header>NACIONALIDAD</header>
-              <span>{{ nacionalidad }}</span>
-            </article>
-            <article>
-              <header>SEXO</header>
-              <span>{{ sexo }}</span>
-            </article>
+          <div class="row">
+            <div class="col-12">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>APELLIDOS</header>
+                <span>{{ apellidos }}</span>
+              </article>
+            </div>
           </div>
-          <div class="grid-2-colums">
-            <article>
-              <header>FECHA DE NACIMIENTO</header>
-              <span>{{ fechaNacimiento }}</span>
-            </article>
-            <article>
-              <header>NÚMERO DOCUMENTO</header>
-              <span>{{ numeroDocumento }}</span>
-            </article>
+          
+          <div class="row">
+            <div class="col-12">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>NOMBRES</header>
+                <span>{{ nombres }}</span>
+              </article>
+            </div>
           </div>
-          <div class="grid-2-colums">
-            <article>
-              <header>FECHA DE EMISIÓN</header>
-              <span>{{ fechaEmision }}</span>
-            </article>
-            <article>
-              <header>FECHA DE VENCIMIENTO</header>
-              <span>{{ fechaVencimiento }}</span>
-            </article>
+
+          <div class="row">
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>NACIONALIDAD</header>
+                <span>{{ nacionalidad }}</span>
+              </article>
+            </div>
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>SEXO</header>
+                <span>{{ sexo }}</span>
+              </article>
+            </div>
           </div>
-          <article>
-            <header>FIRMA DEL TITULAR</header>
-            <span>{{ firma }}</span>
-          </article>
-        </section>
-      </main>
+
+          <div class="row">
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>FECHA DE NACIMIENTO</header>
+                <span>{{ fechaNacimiento }}</span>
+              </article>
+            </div>
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>NÚMERO DOCUMENTO</header>
+                <span>{{ numeroDocumento }}</span>
+              </article>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>FECHA DE EMISIÓN</header>
+                <span>{{ fechaEmision }}</span>
+              </article>
+            </div>
+            <div class="col-6">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>FECHA DE VENCIMIENTO</header>
+                <span>{{ fechaVencimiento }}</span>
+              </article>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <article class="text-start"> <!-- Ensure text is aligned to the left -->
+                <header>FIRMA DEL TITULAR</header>
+                <span>{{ firma }}</span>
+              </article>
+            </div>
+          </div>
+</section>
+
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        run: '22.222.222-2',
-        apellidos: 'MUSK',
-        nombres: 'ELON REEVE',
-        nacionalidad: 'SUDAFRICANO',
-        sexo: 'M',
-        fechaNacimiento: '28 JUN 1971',
-        numeroDocumento: '222.222.222',
-        fechaEmision: '01 ENE 2025',
-        fechaVencimiento: '01 ENE 2030',
-        firma: 'X Æ A-12',
-        photo: '/foto_elon.jpg', // Path to the user photo
-      };
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Include your CSS styles here or import from a CSS file */
-  </style>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      run: '22.222.222-2',
+      apellidos: 'MUSK',
+      nombres: 'ELON REEVE',
+      nacionalidad: 'SUDAFRICANO',
+      sexo: 'M',
+      fechaNacimiento: '28 JUN 1971',
+      numeroDocumento: '222.222.222',
+      fechaEmision: '01 ENE 2025',
+      fechaVencimiento: '01 ENE 2030',
+      firma: 'X Æ A-12',
+    };
+  },
+};
+</script>
+
+<style scoped>
+#chile-flag {
+  height: 2rem;
+}
+
+.header {
+  align-self: flex-start;
+}
+
+.carnet {
+  background: #c8deed;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px 2px black;
+  padding: 15px;
+}
+
+.carnet-aside {
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+}
+
+.carnet-main {
+  text-align: left;
+}
+
+.user-photo {
+  margin-top: 10px;
+  width: auto;
+  height: 13rem;
+  border-radius: 5px;
+}
+
+.run {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+h1 {
+  font-size: 24px; 
+  margin: 0;
+  font-weight: bold;
+  color: #0235a4;
+}
+
+.servicio {
+  font-size: 16px; 
+  font-weight: bold; 
+  color: #0235a4; 
+  margin-top: 5px; 
+}
+
+article header {
+  font-weight: bold;
+  color: #0235a4;
+  font-size: 10px; 
+}
+
+section article span {
+  font-size: 13px;
+  font-weight: 600;
+}
+</style>
